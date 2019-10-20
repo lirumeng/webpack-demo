9cbb8170cf0cadfec92dfc04cf871e61645a464f
lirumeng <meng87743>
2019-10-20 12:25:34 +0800

    output management

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import _ from 'lodash'
+import printMain from './print.js'
 
 function component() {
     var element = document.createElement('div')
+    var btn = document.createElement('button');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-    element.classList.add('hello')
+
+    btn.innerHTML = 'Click me and check the console';
+    btn.onclick = printMain
+
+    element.appendChild(btn)
 
     return element
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
+    plugins: [
+        // 清除构建目标文件夹./dist
+        new CleanWebpackPlugin({
+            default: ['dist']
+        }),
+        // 自动输出./dist/index.html
+        new HtmlWebpackPlugin({
+            title: '输出管理'
+        })
+    ],
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     }
 }
