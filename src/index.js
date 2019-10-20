3c1658e260bd6bfeb801f1196039b6b2942648f6
lirumeng <meng87743>
2019-10-20 11:46:22 +0800

    asset management-rollback

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import _ from 'lodash'
-import './style.css'
-import BgImage from './assets/images/bg.jpg'
-import Data from './assets/xml/demo.xml'
 
 function component() {
     var element = document.createElement('div')
 
     element.innerHTML = _.join(['Hello你好', 'webpack'], ' ')
     element.classList.add('hello')
-
-    var myBg = new Image()
-    myBg.src = BgImage
-    element.appendChild(myBg)
-
-    console.log(Data)
 
     return element
 }
