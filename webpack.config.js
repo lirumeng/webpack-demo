80d42397d5f1a04879631c1519b392772792b315
lirumeng <meng87743>
2019-10-20 11:41:17 +0800

    asset management

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,35 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+                // 加载css文件
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                // 加载图片资源
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader']
+            },
+            {
+                // 加载字体
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(csv|tsv)/,
+                use: ['csv-loader']
+            },
+            {
+                test: /\.xml/,
+                use: ['xml-loader']
+            }
+        ]
     }
 }
